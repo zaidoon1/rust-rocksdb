48d4f1ea37dc9fc503a43c2ca1db405225f3ee6c
zaidoon <zaidoon@cloudflare.com>
2024-03-20 16:27:18 +0000

    fix stderr logger binding

--- src/db_options.rs
+++ src/db_options.rs
@@ -3262,16 +3262,11 @@ impl Options {
 
     /// Prints logs to stderr for faster debugging
     /// See official [wiki](https://github.com/facebook/rocksdb/wiki/Logger) for more information.
-    pub fn set_stderr_logger(&mut self, log_level: LogLevel, prefix: Option<impl CStrLike>) {
-        let mut prefix_ptr = std::ptr::null();
-        if let Some(prefix) = prefix {
-            let prefix = prefix.into_c_string().unwrap();
-            prefix_ptr = prefix.as_ptr();
-        }
+    pub fn set_stderr_logger(&mut self, log_level: LogLevel, prefix: impl CStrLike) {
+        let p = prefix.into_c_string().unwrap();
 
         unsafe {
-            let logger = ffi::rocksdb_logger_create_stderr_logger(log_level as c_int, prefix_ptr);
-
+            let logger = ffi::rocksdb_logger_create_stderr_logger(log_level as c_int, p.as_ptr());
             ffi::rocksdb_options_set_info_log(self.inner, logger);
         }
     }

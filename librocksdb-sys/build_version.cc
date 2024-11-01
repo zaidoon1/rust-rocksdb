6dad602f811259c5d9be89ae7ac9afcd787c10d7
Zaidoon Abd Al Hadi <zaidoon@cloudflare.com>
2024-11-01 13:09:39 -0400

    upgrade to RocksDB 9.7.4

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -8,17 +8,17 @@
 
 // The build script may replace these values with real values based
 // on whether or not GIT is available and the platform settings
-static const std::string rocksdb_build_git_sha  = "0e2801ac30b3f283c3b14e523ba3667eca024f09";
-static const std::string rocksdb_build_git_tag = "rocksdb_build_git_tag:v9.7.3";
+static const std::string rocksdb_build_git_sha  = "3c27a3dde0993210c5cc30d99717093f7537916f";
+static const std::string rocksdb_build_git_tag = "rocksdb_build_git_tag:v9.7.4";
 #define HAS_GIT_CHANGES 0
 #if HAS_GIT_CHANGES == 0
 // If HAS_GIT_CHANGES is 0, the GIT date is used.
 // Use the time the branch/tag was last modified
-static const std::string rocksdb_build_date = "rocksdb_build_date:2024-10-17 12:29:59";
+static const std::string rocksdb_build_date = "rocksdb_build_date:2024-11-01 08:32:35";
 #else
 // If HAS_GIT_CHANGES is > 0, the branch/tag has modifications.
 // Use the time the build was created.
-static const std::string rocksdb_build_date = "rocksdb_build_date:2024-10-17 12:29:59";
+static const std::string rocksdb_build_date = "rocksdb_build_date:2024-11-01 08:32:35";
 #endif
 
 std::unordered_map<std::string, ROCKSDB_NAMESPACE::RegistrarFunc> ROCKSDB_NAMESPACE::ObjectRegistry::builtins_ = {};
